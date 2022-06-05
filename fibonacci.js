3b0fec5736eca6948710943abab84a9264068da9
CHHAM BAHADUR GURUNG <85616587+iamchh@users.noreply.github.com>
2022-06-05 22:35:36 +0545

    program to print fibonacci sequence

---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+//take input from the user
+const num=ParseInt(prompt('Enter the number of terms: '));
+let n1=0,n2=1,nextterm;
+console.log('Fibonacci Series:');
+
+for(i=1,num<= num,i++){
+ console.log(n1);
+ nextterm=n1+n2;
+ n1=n2;
+ n2=nextterm'
+}
