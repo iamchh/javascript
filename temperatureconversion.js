5a08f69cab7bf179ed9c92183456c2c4e4d9ccfb
CHHAM BAHADUR GURUNG <85616587+iamchh@users.noreply.github.com>
2022-06-05 21:10:20 +0545

    Create temperatureconversion.js

---
+++ temperatureconversion.js
@@ -0,0 +1,7 @@
+//program to convert celsius into fahrenheit
+//take input from the user
+const celsius=prompt('Enter the value:');
+//calculate fahrenheit
+const fahrenheit=(celsius*1.8)+32
+//display the output
+console.log('The value of ${celsius} into fahrenheit is ${fahrenheit}');
